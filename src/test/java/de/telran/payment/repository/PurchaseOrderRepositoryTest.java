ded984bce9870524fa363a71fe396181bd7d1ed9
Andrii <vytiaganets@ukr.net>
2024-05-29 17:21:35 +0200

    29.05.2024 refactoring

--- src/test/java/de/telran/payment/repository/PurchaseOrderRepositoryTest.java
+++ src/test/java/de/telran/payment/repository/PurchaseOrderRepositoryTest.java
@@ -47,30 +47,30 @@ class PurchaseOrderRepositoryTest {
 
     @Test
     void testInsert() {
-//        PurchaseOrder purchaseOrderExpected = new PurchaseOrder();
-//        Recipient recipientTest = new Recipient();
-//        purchaseOrderExpected.setOrderId(323132L);
-//        //purchaseOrderExpected.setRecipientId("452542");
-//        purchaseOrderExpected.setType(CARD);
-//        purchaseOrderExpected.setStatus(NEW);
-//        purchaseOrderExpected.setAmount(BigDecimal.valueOf(2321000));
-//        purchaseOrderExpected.setCreatedAt(Timestamp.valueOf("2024-05-25 14:33:02.940000"));
-//        purchaseOrderExpected.setUpdatedAt(Timestamp.valueOf("2024-05-24 14:33:02.941000"));
-//        recipientTest.setRecipient(purchaseOrderExpected);
-//
-//
-//        recipientTest.setId(1L);
-//        purchaseOrderExpected.setRecipient(recipientTest);
-//
-//        Sender senderTest = new Sender();
-//        senderTest.setId(1L);
-//        purchaseOrderExpected.setSender(senderTest);
-//
-//        PurchaseOrder purchaseOrderActual = purchaseOrderTest.save(purchaseOrderExpected);
-//
-//        Assertions.assertNotNull(purchaseOrderActual);
-//        Assertions.assertTrue(purchaseOrderExpected.getId() > 0);
-//        Assertions.assertEquals(purchaseOrderExpected.getId(), purchaseOrderActual.getId());
+        PurchaseOrder purchaseOrderExpected = new PurchaseOrder();
+
+        Recipient recipientTest = new Recipient();
+        recipientTest.setId(1L);
+
+        Sender senderTest = new Sender();
+        senderTest.setId(1L);
+
+        purchaseOrderExpected.setSender(senderTest);
+        purchaseOrderExpected.setRecipient(recipientTest);
+        purchaseOrderExpected.setPaymentId("341343");
+        purchaseOrderExpected.setOrderId(323132L);
+        purchaseOrderExpected.setType(CARD);
+        purchaseOrderExpected.setStatus(NEW);
+        purchaseOrderExpected.setAmount(BigDecimal.valueOf(2321000));
+        purchaseOrderExpected.setCreatedAt(Timestamp.valueOf("2024-05-25 14:33:02.940000"));
+        purchaseOrderExpected.setUpdatedAt(Timestamp.valueOf("2024-05-24 14:33:02.941000"));
+        purchaseOrderExpected.setId(1L);
+
+        PurchaseOrder purchaseOrderActual = purchaseOrderTest.save(purchaseOrderExpected);
+
+        Assertions.assertNotNull(purchaseOrderActual);
+        Assertions.assertTrue(purchaseOrderExpected.getId() > 0);
+        Assertions.assertEquals(purchaseOrderExpected.getId(), purchaseOrderActual.getId());
     }
 
     @Test
